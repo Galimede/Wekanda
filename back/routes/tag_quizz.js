ff3f576f0266584608689381ba105a96d52a1427
christophe_guide <christophe_guide@ens.univ-artois.fr>
2020-05-29 11:49:05 +0200

    add get /id_quizz pour tag_quizz

--- back/routes/tag_quizz.js
+++ back/routes/tag_quizz.js
@@ -8,6 +8,14 @@ router
             res.json(result.rows);
             res.status(200);
         })
+
+    .get('/:id_quizz',
+        async (req, res) => {
+            const result = await pool.query('SELECT tag FROM tagQuizz WHERE id_quizz=$1', [req.params.id_quizz]);
+            res.json(result.rows);
+            res.status(200);
+        })
+
     .post("/", 
         async (req,res) => {
             const result = await pool.query("INSERT INTO tagquizz(id_quizz, tag) values($1,$2)", [req.body.id_quizz,req.body.tag]);
